28e7192bcf650e07f6aee0fabee777108571f339
Ivan <plastun.vanya@yandex.ru>
2019-02-20 12:35:46 +0300

    foinni landing was ready

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+﻿var gulp = require("gulp"),
+    browserSync = require('browser-sync'),
+    less = require('gulp-less'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+
+gulp.task('server', ['less'], function() {
+   browserSync.init({
+      server: {baseDir: "./app/"} 
+   });
+   gulp.watch("app/**/*.html").on("change", browserSync.reload);
+   gulp.watch("app/**/*.js").on("change", browserSync.reload);
+   gulp.watch("app/less/**/*.less", ["less"]);
+});
+
+gulp.task('less', function() {
+    return gulp.src('./app/less/main.less')
+    .pipe(plumber({
+        errorHandler: notify.onError(function(err) {
+            return {
+                title: "less",
+                sound: false,
+                message: err.message
+            }
+        })
+    }))
+   .pipe(less())
+   .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('default', ['server']);
